e38de7349534ca5a73dd42c97af1e66a784ac39c
tstoicescu <tstoicescu@attosoft.ro>
2023-08-16 19:34:27 +0300

    finisaje

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,28 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatListModule} from '@angular/material/list'; 
+
+import { ScrollDispatcher } from '@angular/cdk/scrolling';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, RouterModule],
+  imports: [
+    HomeComponent, RouterModule,
+    MatListModule, MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule ],
   template: `
-    <main>
-      <a [routerLink]="['/']">
+
+
+
+
+  <main>
+  <a [routerLink]="['/']">
         <header class="brand-name">
           <img
             class="brand-logo"
@@ -21,10 +36,16 @@ import { HomeComponent } from './home/home.component';
       <section class="content">
         <router-outlet></router-outlet>
       </section>
-    </main>
+      </main>
+
+
+     
+    
   `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
+
+   constructor(private scroll: ScrollDispatcher) {}
 }
